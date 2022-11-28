1b163d49b3009572ca3e53d943f17d50d48ecfe1
PereRF <pereroca@iesmontsia.org>
2022-11-28 09:01:25 +0100

    Test rama dev

--- RocaOrdenats3.java
+++ RocaOrdenats3.java
@@ -7,8 +7,8 @@ public class RocaOrdenats3 {
     int numero1;
     int numero2;
     boolean ordenat = true;
-    // TEST XAVI
-    //Demanem el primer nombre i el guerdem 
+    // Test Dev
+    //Demanem el primer nombre i el guerdem
     numero1 = scanner.nextInt();
     //Bucle que controle el numero de vegades que comaparem els valors (n-1)
     for(int i=0; i < n-1; i++){
